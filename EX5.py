b0cd1fc1f68dff8d69f93f9835974409ca1c9b88
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-05-14 03:38:15 +0100

    Add files via upload

---
+++ EX5.py
@@ -0,0 +1,12 @@
+def calculer_moyenne():
+    input(nombre de notes:)
+    coef=int
+    total = 0
+    for i in range(n):
+        note = float(input(f"Entrez la note {i + 1}: "))
+        total += note
+    moyenne = total / n
+    return moyenne 
+
+
+
